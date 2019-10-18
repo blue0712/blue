8ef9c0bae541a161f36240e19d9ff8104271a2c2
blue0712 <bluelemon712@gmail.com>
2019-10-18 17:01:37 +0800

    +4

---
+++ demo31_module.py
@@ -0,0 +1,12 @@
+def foo(x, y):
+    print("demo31/foo,value=%d" % (x + y))
+
+
+def bar(x, y):
+    print("demo31/bar,value=%d" % (x * y))
+
+
+
+if __name__ == '__main__':
+    foo(1, 2)
+    bar(3, 4)
